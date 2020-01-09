f05251827464e0072788eefdefefe3736ab237a9
Thomas Eiling <teiling88@gmail.com>
2020-01-09 14:46:13 +0100

    fix pr comments

--- src/FastBillConnector.php
+++ src/FastBillConnector.php
@@ -4,6 +4,8 @@ namespace ShopwareBlog\FastBillConnector;
 
 use Shopware\Core\Framework\Context;
 use Shopware\Core\Framework\DataAbstractionLayer\EntityRepositoryInterface;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsFilter;
 use Shopware\Core\Framework\Plugin;
 use Shopware\Core\Framework\Plugin\Context\InstallContext;
 use Shopware\Core\System\CustomField\CustomFieldTypes;
@@ -14,12 +16,24 @@ class FastBillConnector extends Plugin
     {
         /** @var EntityRepositoryInterface $customFieldSetRepository */
         $customFieldSetRepository = $this->container->get('custom_field_set.repository');
+        $count = $customFieldSetRepository->searchIds(
+            (new Criteria())->addFilter(new EqualsFilter('name', 'swb_fastbill_product')),
+            Context::createDefaultContext()
+        )->getTotal();
+
+        if ($count !== 0) {
+            return;
+        }
+
         $customFieldSetRepository->upsert(
             [
                 [
                     'name' => 'swb_fastbill_product',
                     'customFields' => [
-                        ['name' => 'swb_fastbill_id', 'type' => CustomFieldTypes::INT],
+                        [
+                            'name' => 'swb_fastbill_id',
+                            'type' => CustomFieldTypes::INT,
+                        ],
                     ],
                     'relations' => [
                         ['entityName' => 'product'],
